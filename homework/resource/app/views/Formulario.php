e632c6f892e05f4ea7dd068fe8401e5fced4dabe
Killua010 <Daniel160598@hotmail.com>
2018-05-03 15:34:22 -0300

    Adição do formulario

--- homework/resource/app/views/Formulario.php
+++ homework/resource/app/views/Formulario.php
@@ -1,6 +1,38 @@
 <?php
-
-  echo "Formulario";
-
-
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>HomeWork</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>   
+        <header>
+            <h1>Meus HomeWorks</h1>
+        </header>   
+        <article>
+            <section>
+                <form>
+                    <fieldset>
+                        <legend>Novo HomeWork</legend>
+                        Titulo
+                        <br>
+                        <input type="text" name="titulo" placeholder="Ex: Ambiente de desenvolvimento android">
+                        <br>
+                        Descrição
+                        <br>
+                        <textarea name="descricao" placeholder="Pesquisar sobre o ambiente android. em torno de 10 paginas"></textarea>
+                        <br>
+                        Materia
+                        <br>
+                        <input type="text" name="materia" placeholder="Ex: Sistemas Operacionais II">
+                        <input type="submit" name="Enviar" value="Salvar">
+                    </fieldset>
+                </form>
+            </section>
+            <section>
+                
+            </section>
+        </article>
+    </body>
+</html>
 ?>
